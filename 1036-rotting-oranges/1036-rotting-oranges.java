7624de211828277e23bd2948d04569bbfd93d3e4
Mithilesh Gopalakrishnan S <156875049+mithgx@users.noreply.github.com>
2025-09-26 19:42:52 +0530

    Time: 1 ms (100%), Space: 42.2 MB (86.9%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,68 +1,40 @@
-class Pair{
-    int i;
-    int j;
-    int time;
-    Pair(int i, int j, int time)
-{
-    this.i=i;
-    this.j=j;
-    this.time=time;
-}}
 class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        Queue<Pair> q = new LinkedList<>();
-        int[][] vis = new int[n][m];
+        int[][] time = new int[n][m];
+        for(int [] t : time){
+            Arrays.fill(t,Integer.MAX_VALUE);
+        }
 
+        for(int i=0;i<n;i++ ){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2) dfs(grid,time,i,j,0);
+
+            }
+        }
+
+        int timereq=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    q.add(new Pair(i,j,0));
-                    vis[i][j]=2;
-                }
-                else vis[i][j]=0;
-
-
-
-
-
-            }
-        }
-        int time=0;
-        int cnt=0;
-
-        int[] drow = {-1,0,+1,0};
-        int[] dcol ={0,+1,0,-1};
-
-        while(!q.isEmpty()){
-            int r = q.peek().i;
-            int c = q.peek().j;
-            int t = q.peek().time;
-            time = Math.max(time,t);
-            q.poll();
-
-            for(int i=0;i<4;i++){
-                int nrow = r+drow[i];
-                int ncol = c+dcol[i];
-                if(nrow>=0 && ncol >=0 && nrow<n && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                    q.add(new Pair(nrow,ncol,t+1));
-                    vis[nrow][ncol]=2;
-                    cnt++;
+                if(grid[i][j]==1){
+                    if(time[i][j]==Integer.MAX_VALUE) return -1;
+                    timereq=Math.max(timereq,time[i][j]);
                 }
             }
-
         }
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(vis[i][j]!=2 && grid[i][j]==1) return -1;
-            }
-        }
-        return time;
-
-
-
+        return timereq;
         
+    }
+
+    private void dfs(int[][] grid, int[][] time,int i , int j , int currtime){        int n = grid.length;
+    int m = grid[0].length;
+        if(i<0 || j<0 || i>=n || j>=m ||grid[i][j]==0 ||currtime>=time[i][j]) return;
+        time[i][j]=currtime;
+        dfs(grid,time,i+1,j,currtime+1);
+        dfs(grid,time,i,j+1,currtime+1);
+        dfs(grid,time,i-1,j,currtime+1);
+        dfs(grid,time,i,j-1,currtime+1);
+
     }
 }
