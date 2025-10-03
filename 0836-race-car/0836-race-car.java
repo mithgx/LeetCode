7e5641c26fc1bacee7b10d8218d4faaa8b370017
Mithilesh Gopalakrishnan S <156875049+mithgx@users.noreply.github.com>
2025-10-03 18:44:51 +0530

    Time: 405 ms (20.95%), Space: 129.1 MB (17.14%) - LeetHub

--- 0836-race-car/0836-race-car.java
+++ 0836-race-car/0836-race-car.java
@@ -1,39 +1,35 @@
 class Solution {
     public int racecar(int target) {
-
-        Queue<int[]> q =new LinkedList<>();
-        Set<String> vis =  new HashSet<>();
-        q.offer(new int[]{0,1,0});
-        vis.add("0,1");
+        Queue<int[]> q = new LinkedList<>();
+        Set<String> vis = new HashSet<>();
+        q.add(new int[]{0,1,0});
 
         while(!q.isEmpty()){
-            int[] curr  = q.poll();
-            int pos=curr[0];
-            int speed=curr[1];
+            int[] curr = q.poll();
+            int pos = curr[0];
+            int speed = curr[1];
             int moves = curr[2];
 
             if(pos==target) return moves;
 
-            //accelerate
-
             int nextpos = pos+speed;
-            int nextspeed= speed*2;
-            String next = nextpos+","+nextspeed;
-
-            if(!vis.contains(next) && Math.abs(nextpos)<=2*target){
-                q.offer(new int[]{nextpos,nextspeed,moves+1});
-                vis.add(next);
+            int nextspeed = 2*speed;
+            String A = nextpos+","+nextspeed;
+            if(!vis.contains(A) && Math.abs(nextpos) < 2*target){
+                vis.add(A);
+                q.add(new int[]{nextpos,nextspeed,moves+1});
             }
-
-            //reverse
 
             nextpos = pos;
-            nextspeed = speed>0 ? -1 : 1;
+
+            nextspeed = speed >0 ?  -1:1;
             String B = nextpos+","+nextspeed;
+
             if(!vis.contains(B)){
-                q.offer(new int[]{nextpos,nextspeed,moves+1});
                 vis.add(B);
+                q.add(new int[]{nextpos,nextspeed,moves+1});
             }
+
         }
         return -1;
         
